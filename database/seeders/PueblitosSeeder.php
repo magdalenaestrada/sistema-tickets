9aedcd7fb7fd99d5c07b4c4071cb8749d4562e71
rvnKatt <magadali03@gmail.com>
2026-05-14 06:55:23 -0500

    WEADADS

--- database/seeders/PueblitosSeeder.php
+++ database/seeders/PueblitosSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Pueblito;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,50 @@ class PueblitosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Pueblito::insert([
+            [
+                'distrito_id' => 378,
+                'nombre' => 'CHALA',
+            ],
+            [
+                'distrito_id' => 378,
+                'nombre' => 'ACHANIZO',
+            ],
+            [
+                'distrito_id' => 378,
+                'nombre' => 'TIRUQUE',
+            ],
+            [
+                'distrito_id' => 378,
+                'nombre' => 'MOLINO',
+            ],
+
+            [
+                'distrito_id' => 379,
+                'nombre' => 'CHAPARRA',
+            ],
+            [
+                'distrito_id' => 379,
+                'nombre' => 'ARASQUI',
+            ],
+            [
+                'distrito_id' => 379,
+                'nombre' => 'SIFUENTES',
+            ],
+            [
+                "distrito_id" => 383,
+                "nombre" => 'QUICACHA',
+            ],
+            [
+                "distrito_id" => 383,
+                "nombre" => 'TIERRAS BLANCAS',
+            ]
+                            'LA VICTORIA',
+                'CRUCE SONDOR',
+                'CRUCE CAHUACHO',
+                'CRUCE AYROCA',
+                'CRUCE TARCO',
+
+        ]);
     }
 }
